df719fbd652d266f33cbe3ed20ddb867ee3b17fd
anderson santana nunes <blueoystercult1982@gmail.com>
2021-01-04 21:44:56 -0300

    project created

---
+++ Backend/src/main/java/com/correriadelivery/CorreriaDelivery/CorreriaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.correriadelivery.CorreriaDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreriaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreriaDeliveryApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/correriadelivery/CorreriaDelivery/CorreriaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correriadelivery.CorreriaDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreriaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
