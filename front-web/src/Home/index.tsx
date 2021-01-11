deeb574cfa734aa902640560907cc3021950e5a9
anderson5692 <blueoystercult1982@gmail.com>
2021-01-11 17:55:33 -0300

    home

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+import {ReactComponent as MainImage} from './main.svg'
+
+function Home() {
+    return(
+        <div className="home-container">
+            <div className="home-content">
+            <div className="home-actions">
+                <h1 className="home-title">
+                    Bateu aquela fominha, <br/> peça já no Correria!!!
+                </h1>
+                <h3 className="home-subtitle"> 
+                    Escolha seu pedido e deixa<br/> a correria com a gente
+                </h3>
+                <a href="link pra pg pedidos" className="home-btn-order">
+                    FAZER PEDIDO
+                </a>
+                </div>
+                <div className="home-image">
+                    <MainImage/>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Home;
